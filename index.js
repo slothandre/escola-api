250ecac8fbce9ad6fd4c63ac107770a07783caf5
André Marques <andre.m.d.abreu@gmail.com>
2023-09-27 11:53:39 -0300

    Cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { ler, inserir, lerUm, atualizar, excluir } from "./src/aluno.js"
+import cors from "cors";
+
 const app = express();
 const porta = process.env.PORT || 3306;
 
@@ -8,6 +10,9 @@ app.use(express.json());
 
 // adicionando suporte a dados vindos de formulários
 app.use(express.urlencoded({ extended : true}))
+
+// permitindo acesso aos arquivos da API
+app.use(cors());
 
 // CRIANDO AS ROTAS
 
